bdb691d14e9e8ff2ef36b3ec4d87ed30cfb8369e
unknown <asanchezm12@est.ups.edu.ec>
2022-01-12 23:51:20 -0500

    Version 1.0

--- salarios.cpp
+++ salarios.cpp
@@ -26,7 +26,72 @@ void Salarios::limpiar()
     ui->inHoras->setValue(0);
     ui->inMatutino->setChecked(true);
     ui->inNombre->setFocus();
+}
 
+void Salarios::guardar()
+{
+    // Abrir cuadro de diálogo para seleccionar ubicación y nombre del archivo.
+    QString nombreArchivo = QFileDialog::getSaveFileName(this,
+                                                         "Guardar archivo",
+                                                         QDir::home().absolutePath(),
+                                                         "Archivos de salarios (*.slr)");
+
+    // Crear un objeto QFile
+    QFile archivo(nombreArchivo);
+    // Abrirlo para escritura
+    if(archivo.open(QFile::WriteOnly | QFile::Truncate)){
+        // Crear un 'stream' de texto
+        QTextStream salida(&archivo);
+        // Enviar los datos del resultado a la salida
+        salida << ui->outResultado->toPlainText();
+        // Mostrar 5 segundo que todo fue bien
+        ui->statusbar->showMessage("Datos almacenados en " + nombreArchivo, 5000);
+    }else {
+        // Mensaje de error si no se puede abrir el archivo
+        QMessageBox::warning(this,
+                             "Guardar datos",
+                             "No se pudo guardar el archivo");
+    }
+    // Cerrar el archivo
+    archivo.close();
+
+}
+
+void Salarios::abrir()
+{
+    // Abrir cuadro de diálogo para seleccionar ubicación y nombre del archivo.
+    QString nombreArchivo = QFileDialog::getOpenFileName(this,
+                                                         "Abrir archivo",
+                                                         QDir::home().absolutePath(),
+                                                         "Archivos de salarios (*.slr)");
+
+    // Crear un objeto QFile
+    QFile archivo(nombreArchivo);
+    // Abrirlo para lectura
+    if(archivo.open(QFile::ReadOnly)){
+        // Crear un 'stream' de texto
+        QTextStream entrada(&archivo);
+        // Leer todo el contenido del archivo
+        QString datos = entrada.readAll();
+        // Cargar el contenido al área de texto
+        ui->outResultado->clear();
+        ui->outResultado->setPlainText(datos);
+        // Mostrar 5 segundo que todo fue bien
+        ui->statusbar->showMessage("Datos leidos desde " + nombreArchivo, 5000);
+    }else {
+        // Mensaje de error si no se puede abrir el archivo
+        QMessageBox::warning(this,
+                             "Abrir datos",
+                             "No se pudo abrir el archivo");
+    }
+    // Cerrar el archivo
+    archivo.close();
+}
+
+
+void Salarios::on_actionCalcular_triggered()
+{
+    calcular();
 }
 
 void Salarios::calcular()
@@ -42,14 +107,16 @@ void Salarios::calcular()
     } else {
         jornada = TipoJornada::Nocturna;
     }
-    //Validar datos correctos
-    if(nombre=="" || horas==0){
-        /*QMessageBox msgBox;
-        msgBox.setText("El nombre o el numero de horas esta vacio");
-        msgBox.setIcon(QMessageBox::Warning);
-        msgBox.exec(); */
 
-        QMessageBox::warning(this,"Advertencia","EL nombre o el numero de horas esta vacio");
+    // Validar datos correctos
+    if (nombre == "" || horas == 0){
+        /*
+        QMessageBox msgBox;
+        msgBox.setText("El nombre o el número de horas está vacío");
+        msgBox.setIcon(QMessageBox::Warning);
+        msgBox.exec();
+        */
+        QMessageBox::warning(this,"Advertencia","El nombre o el número de horas está vacío");
         return;
     }
 
@@ -57,78 +124,18 @@ void Salarios::calcular()
     m_controlador->agregarObrero(nombre, horas, jornada);
     // Calcular
     if (m_controlador->calcularSalario()){
-        //muestra los resultados de los calculos del obrero
+        // muestra los resultados de los calculos del obrero
         ui->outResultado->appendPlainText(m_controlador->obrero()->toString());
-        //limpiar la interfaz
+        // limpiar la interfaz
         limpiar();
-        //Mostrar mensaje por 5 segundos en la barra de estado
-        ui->statusbar->showMessage("Calculos procesados para " + nombre,5000);
+        // Mostrar mensaje por 5 segundos en la barra de estado
+        ui->statusbar->showMessage("calculos procesados para " + nombre, 5000);
+    }else {
+        QMessageBox::critical(
+                    this,
+                    "Error",
+                    "Error al calcular el salario.");
     }
-    else{
-        QMessageBox::critical(this,
-                              "Error",
-                              "Error al calcular el salario.");
-    }
-}
-
-void Salarios::guardar()
-{
-    //Abrir cuadro de dialogo para seleccionar ubicacion y nombre del archivo.
-    QString  nombreArchivo = QFileDialog::getSaveFileName(this,
-                                                          "Guardar datos",
-                                                          QDir::home().absolutePath(),
-                                                          "Archivos de texto (*.txt)");
-
-
-    //Crear un objet QFile
-    QFile archivo(nombreArchivo);
-    if(archivo.open(QFile::WriteOnly | QFile::Truncate)){
-        //Crear un strem de texto
-        QTextStream salida(&archivo);
-        //Enviar los datos de datos del resultado a la salida
-        salida << ui->outResultado->toPlainText();
-        //Mostrar 5 segundos que todo esta bien
-        ui->statusbar->showMessage("datos almacenados en " + nombreArchivo,5000);
-    }else{
-        QMessageBox::warning(this,
-                             "Guardar datos",
-                             "No se pudo guardar los datos");
-    }
-    archivo.close();
-}
-
-void Salarios::abrir()
-{
-    QString  nombreArchivo = QFileDialog::getOpenFileName(this,
-                                                          "Abrir datos",
-                                                          QDir::home().absolutePath(),
-                                                          "Archivos de texto (*.slr)");
-
-
-    //Crear un objet QFile
-    QFile archivo(nombreArchivo);
-    if(archivo.open(QFile::ReadOnly)){
-        //Crear un strem de texto
-        QTextStream entrada(&archivo);
-         QString datos=entrada.readAll();
-        //Enviar los datos de datos del resultado a la salida
-         ui->outResultado->clear();
-         ui->outResultado->setPlainText(datos);
-        //Mostrar 5 segundos que todo esta bien
-        ui->statusbar->showMessage("Datos leidos desde " + nombreArchivo,5000);
-    }else{
-        QMessageBox::warning(this,
-                             "Abrir datos",
-                             "No se pudo abrir el archivo");
-    }
-    archivo.close();
-}
-
-void Salarios::on_actionNuevo_triggered()
-{
-
-    limpiar();
-        ui->outResultado->clear();
 }
 
 
@@ -138,9 +145,10 @@ void Salarios::on_actionGuardar_triggered()
 }
 
 
-void Salarios::on_actionCalcular_triggered()
+void Salarios::on_actionNuevo_triggered()
 {
-    calcular();
+    limpiar();
+    ui->outResultado->clear();
 }
 
 
@@ -149,3 +157,15 @@ void Salarios::on_actionAbrir_triggered()
     abrir();
 }
 
+
+void Salarios::on_actionAcerca_de_Salarios_triggered()
+{
+    // Crear un objeto de la ventana que queremos invocar
+    Acerca *dialogo = new Acerca(this);
+    // Enviar parámetro a la ventana
+    dialogo->setVersion(VERSION);
+    // Mostrar la ventana (diálogo) MODAL
+    dialogo->exec();
+    // Obtener datos de la ventana
+    qDebug() << dialogo->valor();
+}
--- salarios.h
+++ salarios.h
@@ -2,11 +2,14 @@
 #define SALARIOS_H
 
 #include <QMainWindow>
+#include <QMessageBox>
 #include <QFileDialog>
 #include <QDebug>
-#include "controlador.h"
 
-#include <QMessageBox>
+#include "controlador.h"
+#include "acerca.h"
+
+#define VERSION "1.0"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Salarios; }
@@ -23,19 +26,21 @@ public:
 private slots:
     void on_cmdCalcular_clicked();
 
+    void on_actionCalcular_triggered();
+
     void on_actionGuardar_triggered();
 
-    void on_actionCalcular_triggered();
+    void on_actionNuevo_triggered();
 
     void on_actionAbrir_triggered();
 
-    void on_actionNuevo_triggered();
+    void on_actionAcerca_de_Salarios_triggered();
 
 private:
     Ui::Salarios *ui;
     Controlador *m_controlador;
-    void limpiar();
     void calcular();
+    void limpiar();
     void guardar();
     void abrir();
 };
